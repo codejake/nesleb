56a42caad77a435e584288e9d440a6ca4478d7a0
Jake <codejake@users.noreply.github.com>
2025-01-17 14:00:08 -0800

    changes

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 # Uses maxmind.com databases (free)
 import geoip2.database
 
-# This is super sloppy, sorry. You should know how to Python in order to use 
+# This is super sloppy, sorry. You should know how to Python in order to use
 # this. Feel free to bug me in the usual spots if you have a question.
 with (
     geoip2.database.Reader('./GeoLite2-City.mmdb') as city_reader,
@@ -15,6 +15,7 @@ with (
         try:
             cr = city_reader.city(ip)
             ar = asn_reader.asn(ip)
-            print(f"{ip},{ar.autonomous_system_number},{ar.autonomous_system_organization},{cr.city.name},{cr.subdivisions.most_specific.name},{cr.country.iso_code}")
+            print(f"{ip},{ar.autonomous_system_number},{ar.autonomous_system_organization},{cr.city.name},"
+                  f"{cr.subdivisions.most_specific.name},{cr.country.iso_code}")
         except geoip2.errors.AddressNotFoundError:
             print(f"{ip}: Unknown")
