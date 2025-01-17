46f8b2b7f0c3feab43a70778ba7a2688dc7ec94a
Jake <codejake@users.noreply.github.com>
2025-01-17 13:52:01 -0800

    changes

--- generate.py
+++ generate.py
@@ -5,14 +5,16 @@ import geoip2.database
 
 # This is super sloppy, sorry. You should know how to Python in order to use 
 # this. Feel free to bug me in the usual spots if you have a question.
-with geoip2.database.Reader('./GeoLite2-City.mmdb') as reader:
-    with geoip2.database.Reader('./GeoLite2-ASN.mmdb') as areader:
-        with open("ips.txt") as f:
-            for line in f:
-                ip = line.strip()
-                try:
-                    response = reader.city(ip)
-                    ar = areader.asn(ip)
-                    print(f"{ip},{ar.autonomous_system_number},{ar.autonomous_system_organization},{response.city.name},{response.subdivisions.most_specific.name},{response.country.iso_code}")
-                except geoip2.errors.AddressNotFoundError:
-                    print(f"{ip}: Unknown")
+with (
+    geoip2.database.Reader('./GeoLite2-City.mmdb') as city_reader,
+    geoip2.database.Reader('./GeoLite2-ASN.mmdb') as asn_reader,
+    open("ips.txt") as f
+):
+    for line in f:
+        ip = line.strip()
+        try:
+            cr = city_reader.city(ip)
+            ar = asn_reader.asn(ip)
+            print(f"{ip},{ar.autonomous_system_number},{ar.autonomous_system_organization},{cr.city.name},{cr.subdivisions.most_specific.name},{cr.country.iso_code}")
+        except geoip2.errors.AddressNotFoundError:
+            print(f"{ip}: Unknown")
